a1f870a562ba2c0082b035cae15aaa999c8dac43
Kris1027 <obarzanek.work@gmail.com>
2024-02-08 18:08:39 +0100

    build index, mainlayout and nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export default function Nav() {
+  const [toggled, setToggled] = useState(false);
+  return (
+    <nav className="relative mx-8 mb-24">
+      <svg
+        width="250"
+        height={4}
+        viewBox="0 0 250 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2 2L428 2"
+          strokeWidth={2}
+          stroke="#282828"
+          strokeLinecap="round"
+        />
+      </svg>
+      <div>
+        <img src="/avatar.png" alt="Profile picture of Hua" />
+      </div>
+      <h1>
+        <a href="/">Hua</a>
+      </h1>
+    </nav>
+  );
+}
