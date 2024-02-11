d5129ba3035baa6602aeb99e1fd57190839aed57
Kris1027 <obarzanek.work@gmail.com>
2024-02-11 17:46:03 +0100

    build intro component

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useMediaQuery } from "../util/useMediaQuery";
+import { useMediaQuery } from "../../util/useMediaQuery";
 import { motion } from "framer-motion";
 
 const navMotion = {
